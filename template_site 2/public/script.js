e765cc200c6f15c6c32c62c00b2a64c49d9e982d
AntoineCuinet <118839063+AntoineCuinet@users.noreply.github.com>
2023-11-27 10:39:48 +0100

    Add files via upload

---
+++ template_site 2/public/script.js
@@ -0,0 +1,3 @@
+/* 'as' permet de changer le nom, nomDefault est l'export par défaut                                */
+/* Ceci permet d'inporter ici les fichiers necessaires afin de couper le code en plusieurs fichiers */
+//import nomDefault, {nomExport1 as newName, nomExport2} from "./assets/scripts/nomFichier.js"
