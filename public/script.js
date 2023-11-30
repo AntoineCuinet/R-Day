9fa273387639a09ce87dd4e28983ae8ba879c370
AntoineCuinet <antoine.cuinetdu70200@gmail.com>
2023-12-01 00:00:26 +0100

    add nav-bar + responsive

--- public/script.js
+++ public/script.js
@@ -1,3 +1,26 @@
 /* 'as' permet de changer le nom, nomDefault est l'export par défaut                                */
 /* Ceci permet d'inporter ici les fichiers necessaires afin de couper le code en plusieurs fichiers */
 //import nomDefault, {nomExport1 as newName, nomExport2} from "./assets/scripts/nomFichier.js"
+
+
+/* nav-bar (hamburger) */
+const hamburgerToggler = document.querySelector(".hamburger");
+const navLinksContainer = document.querySelector(".navlinks-container");
+
+const toggleNav = () => {
+    hamburgerToggler.classList.toggle("open");
+
+    const ariaToggle = hamburgerToggler.getAttribute("aria-expanded") === "true" ? "false" : "true";
+    hamburgerToggler.setAttribute("aria-expanded", ariaToggle);
+
+    navLinksContainer.classList.toggle("open");
+}
+hamburgerToggler.addEventListener("click", toggleNav);
+
+new ResizeObserver(entries => { //gérer la transition du menu hamburger
+    if(entries[0].contentRect.width <= 800){
+        navLinksContainer.style.transition = "transform 0.3s ease-out";
+    } else {
+        navLinksContainer.style.transition = "none";
+    }
+}).observe(document.body);
